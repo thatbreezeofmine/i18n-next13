63d2ea980668a797bd8367c0058417bbd482e061
CHABCHI Nassim <57442076+thatbreezeofmine@users.noreply.github.com>
2023-07-04 16:32:06 +0100

    updated

--- i18n.ts
+++ i18n.ts
@@ -1,57 +1,65 @@
 export const defaultLocale = "en-US";
-export const locales = ["en-US", "en-CA", "fr-CA"] as const;
+export const defaultLocales = ["en-US", "en-CA"] as const;
+export const locales = ["en-US", "en-CA", "fr-CA", "fr-FR", "fr-BE"] as const;
 export type ValidLocale = typeof locales[number];
 
 type PathnameLocale = {
-  pathname: string;
-  locale?: never;
+    pathname: string;
+    locale?: never;
 };
 type ISOLocale = {
-  pathname?: never;
-  locale: string;
+    pathname?: never;
+    locale: string;
 };
 
 type LocaleSource = PathnameLocale | ISOLocale;
 
 export const getLocalePartsFrom = ({ pathname, locale }: LocaleSource) => {
-  if (locale) {
-    const localeParts = locale.toLowerCase().split("-");
-    return {
-      lang: localeParts[0],
-      country: localeParts[1],
-    };
-  } else {
-    const pathnameParts = pathname!.toLowerCase().split("/");
-    return {
-      lang: pathnameParts[1],
-      country: pathnameParts[2],
-    };
-  }
+    if (locale) {
+        const localeParts = locale.toLowerCase().split("-");
+        return {
+            country: localeParts[1],
+            lang: localeParts[0],
+        };
+    } else {
+        const pathnameParts = pathname!.toLowerCase().split("/");
+
+        return {
+            country: pathnameParts[1],
+            lang: pathnameParts[2],
+        };
+
+    }
 };
 
-const dictionaries: Record<ValidLocale, any> = {
-  "en-US": () =>
-    import("dictionaries/en-US.json").then((module) => module.default),
-  "en-CA": () =>
-    import("dictionaries/en-CA.json").then((module) => module.default),
-  "fr-CA": () =>
-    import("dictionaries/fr-CA.json").then((module) => module.default),
+//create a dictionary for each locale
+export const dictionaries: Record<ValidLocale, any> = {
+    "en-US": () =>
+        import("dictionaries/en-US.json").then((module) => module.default),
+    "en-CA": () =>
+        import("dictionaries/en-CA.json").then((module) => module.default),
+    "fr-CA": () =>
+        import("dictionaries/fr-CA.json").then((module) => module.default),
+    "fr-FR": () =>
+        import("dictionaries/fr-FR.json").then((module) => module.default),
+    "fr-BE": () =>  
+        import("dictionaries/fr-BE.json").then((module) => module.default),
 } as const;
 
 export const getTranslator = async (locale: ValidLocale) => {
-  const dictionary = await dictionaries[locale]();
-  return (key: string, params?: { [key: string]: string | number }) => {
-    let translation = key
-      .split(".")
-      .reduce((obj, key) => obj && obj[key], dictionary);
-    if (!translation) {
-      return key;
-    }
-    if (params && Object.entries(params).length) {
-      Object.entries(params).forEach(([key, value]) => {
-        translation = translation!.replace(`{{ ${key} }}`, String(value));
-      });
-    }
-    return translation;
-  };
+    const dictionary = await dictionaries[locale]();
+    return (key: string, params?: { [key: string]: string | number }) => {
+        let translation = key
+            .split(".")
+            .reduce((obj, key) => obj && obj[key], dictionary);
+        if (!translation) {
+            return key;
+        }
+        if (params && Object.entries(params).length) {
+            Object.entries(params).forEach(([key, value]) => {
+                translation = translation!.replace(`{{ ${key} }}`, String(value));
+            });
+        }
+        return translation;
+    };
 };
--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,10 @@ import { NextRequest, NextResponse } from "next/server";
 
 import langParser from "accept-language-parser";
 
-import { defaultLocale, locales, getLocalePartsFrom } from "./i18n";
+import { defaultLocale, locales, getLocalePartsFrom, defaultLocales } from "./i18n";
+import { get } from "http";
 
-const findBestMatchingLocale = (acceptLangHeader: string) => {
+const findBestMatchingLocale = (acceptLangHeader: string, currentPathnameParts: any) => {
   // parse the locales acceptable in the header, and sort them by priority (q)
   const parsedLangs = langParser.parse(acceptLangHeader);
 
@@ -24,12 +25,12 @@ const findBestMatchingLocale = (acceptLangHeader: string) => {
     }
     // if we didn't find a match for both language and country, try just the language
     else {
-      const matchedLanguage = locales.find((locale) => {
+      const matchedCountry = defaultLocales.find((locale) => {
         const localeParts = getLocalePartsFrom({ locale });
-        return parsedLang.code === localeParts.lang;
+        return currentPathnameParts.country === localeParts.country;
       });
-      if (matchedLanguage) {
-        return matchedLanguage;
+      if (matchedCountry) {
+        return matchedCountry;
       }
     }
   }
@@ -40,52 +41,120 @@ const findBestMatchingLocale = (acceptLangHeader: string) => {
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
 
-  const defaultLocaleParts = getLocalePartsFrom({ locale: defaultLocale });
+  var defaultLocaleParts = getLocalePartsFrom({ locale: defaultLocale });
   const currentPathnameParts = getLocalePartsFrom({ pathname });
 
   // Check if the default locale is in the pathname
-  if (
-    currentPathnameParts.lang === defaultLocaleParts.lang &&
-    currentPathnameParts.country === defaultLocaleParts.country
-  ) {
-    // we want to REMOVE the default locale from the pathname,
-    // and later use a rewrite so that Next will still match
-    // the correct code file as if there was a locale in the pathname
-    return NextResponse.redirect(
-      new URL(
-        pathname.replace(
-          `/${defaultLocaleParts.lang}/${defaultLocaleParts.country}`,
-          pathname.startsWith("/") ? "/" : ""
-        ),
-        request.url
-      )
-    );
-  }
+    if (
+      currentPathnameParts.lang === defaultLocaleParts.lang &&
+      currentPathnameParts.country === defaultLocaleParts.country
+    ) {
+      // we want to REMOVE the default locale from the pathname,
+      // and later use a rewrite so that Next will still match
+      // the correct code file as if there was a locale in the pathname
+        return NextResponse.redirect(
+          new URL(
+            pathname.replace(
+              `/${defaultLocaleParts.country}/${defaultLocaleParts.lang}`,
+              pathname.startsWith("/") ? "/" : ""
+            ),
+            request.url
+          )
+        );
+    }
+
+    var defaultLang: any = defaultLocales.filter((locale) => {
+      const mylocaleParts = getLocalePartsFrom({ locale });
+      return (
+        currentPathnameParts.country === mylocaleParts.country
+      );
+    });
+
+    if (defaultLang.length > 0) {
+      const matchedLocaleParts = getLocalePartsFrom({ locale: defaultLang[0] })
+
+      if (matchedLocaleParts.lang === currentPathnameParts.lang) {
+        return NextResponse.redirect(
+          new URL(
+            `/${matchedLocaleParts.country}${pathname.replace("/" + matchedLocaleParts.country + "/" + matchedLocaleParts.lang, "")}`,
+            request.url
+          )
+        );
+      }
+    }
+
 
   const pathnameIsMissingValidLocale = locales.every((locale) => {
     const localeParts = getLocalePartsFrom({ locale });
-    return !pathname.startsWith(`/${localeParts.lang}/${localeParts.country}`);
+
+    return !pathname.startsWith(`/${localeParts.country}/${localeParts.lang}`);
   });
 
   if (pathnameIsMissingValidLocale) {
-    // rewrite it so next.js will render `/` as if it was `/en/us`
-
+    // rewrite it so next.js will render `/` as if it was `/us/en`
     const matchedLocale = findBestMatchingLocale(
-      request.headers.get("Accept-Language") || defaultLocale
+      request.headers.get("Accept-Language") || defaultLocale, currentPathnameParts
     );
+
+    const localeParts = getLocalePartsFrom({ locale: matchedLocale });
+    if (pathname.startsWith(`/${localeParts.country}`)) {
+      var defaultLang: any = defaultLocales.filter((locale) => {
+        const mylocaleParts = getLocalePartsFrom({ locale });
+        return (
+          localeParts.country === mylocaleParts.country
+        );
+      });
+
+      if (defaultLang.length > 0) {
+        const matchedLocaleParts = getLocalePartsFrom({ locale: defaultLang[0] })
+        if (matchedLocaleParts.country === getLocalePartsFrom({ locale: defaultLocale }).country) {
+          return NextResponse.redirect(
+            new URL(
+              `/${matchedLocaleParts.country}/${matchedLocaleParts.lang}${pathname.replace("/" + matchedLocaleParts.country, "")}`,
+              request.url
+            )
+          );
+        }
+        return NextResponse.rewrite(
+          new URL(
+            `/${matchedLocaleParts.country}/${matchedLocaleParts.lang}${pathname.replace("/" + matchedLocaleParts.country, "")}`,
+            request.url
+          )
+        );
+      }
+    }
 
     if (matchedLocale !== defaultLocale) {
       const matchedLocaleParts = getLocalePartsFrom({ locale: matchedLocale });
+
+      var defaultLang: any = defaultLocales.filter((locale) => {
+        const mylocaleParts = getLocalePartsFrom({ locale });
+        return (
+          matchedLocaleParts.country === mylocaleParts.country
+        );
+      });
+
+      const localeParts = getLocalePartsFrom({ locale: defaultLang[0] });
+      console.log(localeParts.lang, matchedLocaleParts.lang)
+      if (localeParts.lang == matchedLocaleParts.lang) {
+        return NextResponse.redirect(
+          new URL(
+            `/${matchedLocaleParts.country}${pathname}`,
+            request.url
+          )
+        );
+      }
+
       return NextResponse.redirect(
         new URL(
-          `/${matchedLocaleParts.lang}/${matchedLocaleParts.country}${pathname}`,
+          `/${matchedLocaleParts.country}/${matchedLocaleParts.lang}${pathname}`,
           request.url
         )
       );
     } else {
       return NextResponse.rewrite(
         new URL(
-          `/${defaultLocaleParts.lang}/${defaultLocaleParts.country}${pathname}`,
+          `/${defaultLocaleParts.country}/${defaultLocaleParts.lang}${pathname}`,
           request.url
         )
       );
